a6e61b6680d54d6d0a4cb5d1bc517c390b7451ef
SergioScata <sergiopio395@gmail.com>
2024-01-24 10:11:47 +0100

    Initial commit

---
+++ src/test/java/org/lesson/projectwork/ProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.projectwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
