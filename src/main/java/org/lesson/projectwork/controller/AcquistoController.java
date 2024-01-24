03d85d1deabe057b017f8107b874a4c8f07abdf2
AlesDod02 <alessandro.dod@gmail.com>
2024-01-24 12:08:00 +0100

    adding Acquisto Controller

--- src/main/java/org/lesson/projectwork/controller/AcquistoController.java
+++ src/main/java/org/lesson/projectwork/controller/AcquistoController.java
@@ -1,15 +1,36 @@
 package org.lesson.projectwork.controller;
 
+import org.lesson.projectwork.model.Prodotto;
 import org.lesson.projectwork.repository.AcquistoRepository;
+import org.lesson.projectwork.repository.ProdottoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/shop")
 public class AcquistoController {
     @Autowired
     private AcquistoRepository acquistoRepository;
+    @Autowired
+    private ProdottoRepository prodottoRepository;
+    @GetMapping
+    public String index(@RequestParam(name = "keyword", required = false) String searchKeyword, Model model) {
+        List<Prodotto> prodottoList;
+        if (searchKeyword != null) {
+            prodottoList = prodottoRepository.findByNomeContaining(searchKeyword);
+        } else {
+            prodottoList = prodottoRepository.findAll();
+        }
+        model.addAttribute("prodottoList", prodottoList);
+        model.addAttribute("preloadSearch", searchKeyword);
+        return "shop/list";
+    }
 
 
 }
