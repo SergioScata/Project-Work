dcaabab26d8c70f6622b4fb6f531b393704b3c89
SergioScata <sergiopio395@gmail.com>
2024-01-24 10:29:26 +0100

    Added Prodotto

--- src/main/java/org/lesson/projectwork/model/Prodotto.java
+++ src/main/java/org/lesson/projectwork/model/Prodotto.java
@@ -1,9 +1,73 @@
 package org.lesson.projectwork.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
 
 @Table
 @Entity
 public class Prodotto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String foto;
+    @NotEmpty
+    private String nome;
+    private String descrizione;
+    @NotNull
+    @Min(1)
+    private BigDecimal prezzo;
+    private Integer quantità;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public BigDecimal getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(BigDecimal prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public Integer getQuantità() {
+        return quantità;
+    }
+
+    public void setQuantità(Integer quantità) {
+        this.quantità = quantità;
+    }
 }
