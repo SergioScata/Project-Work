e31b41bebe0468e837fb4412100a2ba44e86b8bf
AlesDod02 <alessandro.dod@gmail.com>
2024-01-31 12:22:43 +0100

    working on registration user

--- src/main/java/org/lesson/projectwork/controller/UserController.java
+++ src/main/java/org/lesson/projectwork/controller/UserController.java
@@ -1,17 +1,24 @@
 package org.lesson.projectwork.controller;
 
+import jakarta.validation.Valid;
 import org.lesson.projectwork.model.Acquisto;
 import org.lesson.projectwork.model.MuseumUser;
+import org.lesson.projectwork.model.Role;
 import org.lesson.projectwork.repository.AcquistoRepository;
 import org.lesson.projectwork.repository.MuseumUserRepository;
 import org.lesson.projectwork.security.DatabaseUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Controller
@@ -21,6 +28,8 @@ public class UserController {
     private AcquistoRepository acquistoRepository;
     @Autowired
     MuseumUserRepository museumUserRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @GetMapping
     public String detail(Model model, Authentication authentication){
         DatabaseUserDetails loggedUser = (DatabaseUserDetails) authentication.getPrincipal();
@@ -36,7 +45,28 @@ public class UserController {
         model.addAttribute("acquisti", acquisti);
         return "user/acquisti";
     }
+    @GetMapping("/register")
+    public String create(Model model) {
+        MuseumUser user = new MuseumUser();
+        model.addAttribute("user", user);
+        return "home/register";
+    }
 
+    @PostMapping("/register")
+    public String create2(@Valid @ModelAttribute("user") MuseumUser formUser, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("user", museumUserRepository.findAll());
+            return "home/register";
+        }
+        Set<Role> userRole= new HashSet<>();
+        Role userRole1= new Role();
+        userRole.add(userRole1);
+        userRole1.setName("USER");
+        formUser.setPassword("{noop}"+formUser.getPassword());
+        formUser.setRoleSet(userRole);
+        MuseumUser savedUser = museumUserRepository.save(formUser);
+        return "redirect:/login";
+    }
 
 
 }
