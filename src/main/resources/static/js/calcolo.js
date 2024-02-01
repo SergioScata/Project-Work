602e114571e867f0a186bdbd04a29ce3bc7b5628
AlesDod02 <alessandro.dod@gmail.com>
2024-02-01 12:10:36 +0100

    fixed function totalCalc js

--- src/main/resources/static/js/calcolo.js
+++ src/main/resources/static/js/calcolo.js
@@ -7,12 +7,12 @@ function totalCalc() {
   nProdotti = parseInt(nProdotti);
   console.log(nProdotti);
 
-  let prezzoSingolo = document.getElementById("prezzoSingolo").getAttribute("th:value");
+  let prezzoSingolo = document.getElementById("prezzoSingolo").textContent;
    console.log(prezzoSingolo);
   prezzoSingolo = parseFloat(prezzoSingolo);
   console.log(prezzoSingolo);
 
-  let prezzoTotale = nProdotti*1;
+  let prezzoTotale = nProdotti*prezzoSingolo;
   prezzoTotale = prezzoTotale.toFixed(2);
   console.log(prezzoTotale);
   document.getElementById("totalPrice").innerHTML=`${prezzoTotale} €`
