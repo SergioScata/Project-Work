c631c0051439b1f44b9bfc4e675da9dfb6878242
MatteoFarinazzo <matteofary@gmail.com>
2024-02-01 12:33:43 +0100

    almost done

--- src/main/resources/static/js/calcolo.js
+++ src/main/resources/static/js/calcolo.js
@@ -5,15 +5,11 @@ document.getElementById("quantita").addEventListener("change", totalCalc);
 function totalCalc() {
   let nProdotti = document.getElementById("quantita").value;
   nProdotti = parseInt(nProdotti);
-  console.log(nProdotti);
 
-  let prezzoSingolo = document.getElementById("prezzoSingolo").getAttribute("th:value");
-   console.log(prezzoSingolo);
+  let prezzoSingolo = document.getElementById("prezzoSingolo").textContent;
   prezzoSingolo = parseFloat(prezzoSingolo);
-  console.log(prezzoSingolo);
 
-  let prezzoTotale = nProdotti*1;
+  let prezzoTotale = nProdotti*prezzoSingolo;
   prezzoTotale = prezzoTotale.toFixed(2);
-  console.log(prezzoTotale);
   document.getElementById("totalPrice").innerHTML=`${prezzoTotale} €`
 }
